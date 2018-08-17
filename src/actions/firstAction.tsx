a56cdf962a1dde73889957ea2a3cab5580cf6acc
tomekg007 <tomaszgubernat7@gmail.com>
2018-08-17 21:45:51 +0200

    add typescript to action

--- src/actions/firstAction.tsx
+++ src/actions/firstAction.tsx
@@ -1,8 +1,12 @@
 const ADD_TODO = 'ADD_TODO';
 
-function addTodo(text: string) {
+type AddTodo ={type: typeof ADD_TODO, text:string};
+
+function addTodo(text: string) : AddTodo {
     return {
       type: ADD_TODO,
       text
     }
   }
+
+  export type Actions = AddTodo;
